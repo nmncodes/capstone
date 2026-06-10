ff4870e3767120f19c66b2c3034ba9d550751367
newman05 <namansurana4566@gmail.com>
2026-06-10 12:15:58 +0530

    make model db

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
+    path('get_cars', views.get_cars, name='getcars'),
 
     # path for add a review view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import login, authenticate, logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from .models import CarMake, CarModel
+from .populate import initiate
+
 import logging
 import json
 
@@ -80,6 +83,23 @@ def registration(request):
     })
 
 
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+
+    if count == 0:
+        initiate()
+
+    car_models = CarModel.objects.select_related('car_make')
+
+    cars = []
+
+    for car_model in car_models:
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+
+    return JsonResponse({"CarModels": cars})
 # Future dealership views
 # def get_dealerships(request):
 #     pass

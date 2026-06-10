d172cd5dd9420b292839ce317b8ebaca1f8d0d55
newman05 <namansurana4566@gmail.com>
2026-06-10 13:57:21 +0530

    req

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,4 +1,4 @@
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -12,6 +12,23 @@ sentiment_analyzer_url = os.getenv(
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
+def get_request(endpoint, **kwargs):
+    params = ""
+
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + str(value) + "&"
+
+    request_url = backend_url + endpoint + "?" + params
+
+    print("GET from {}".format(request_url))
+
+    try:
+        response = requests.get(request_url)
+        return response.json()
+
+    except Exception:
+        print("Network exception occurred")
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text

cc2904fb88669be3cecda7b112f1b0a56603cff7
newman05 <namansurana4566@gmail.com>
2026-06-11 12:21:33 +0530

    fix

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -39,11 +39,11 @@ def analyze_review_sentiments(text):
         return response.json()
 
     except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
+        print(f"Unexpected error {err=}, {type(err)=}")
         print("Network exception occurred")
         return None
-# Add code for retrieving sentiments
 
+# post review
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
 
